5ad61a4721de4643a92dc2be300d0d64468b4258
IrALm <moiseaganze76@gmail.com>
2026-02-27 14:18:07 +0100

    initialisation des repository

---
+++ src/main/java/com/campusRoom/api/repository/RoomRepository.java
@@ -0,0 +1,9 @@
+package com.campusRoom.api.repository;
+
+import com.campusRoom.api.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room , Long> {
+}
